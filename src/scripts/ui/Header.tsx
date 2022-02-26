adcb1f5381441729c76f531fc5a4ed47d87d5ccf
NerdFoundry Gaming <nerdfoundrygaming@gmail.com>
2022-02-26 06:42:19 +0000

    Port to esbuild:
    * Remove all references to parcel, including deps
    * Added esbuild, and plugins needed to build output
    * Added globals.d.ts for jpg/png namespace for imports via esbuild
    * Created esbuild build script for dev/prod builds
    * Minor tweaks here n there

--- src/scripts/ui/Header.tsx
+++ src/scripts/ui/Header.tsx
@@ -1,13 +1,12 @@
+import ImageLogo from '-/assets/logo.png';
 import { Group, Header as MHeader, Image, Title, type HeaderProps } from '@mantine/core';
 import React from 'react';
-
-const ImageLogo = new URL('../../assets/logo.png', import.meta.url);
 
 export default function Header(props: Omit<HeaderProps, 'children'>) {
   return (
     <MHeader {...props}>
       <Group>
-        <Image src={ImageLogo.href} width={48} />
+        <Image src={ImageLogo} width={48} />
 
         <Group align="baseline">
           <Title order={1}>Project: Golden Hammer</Title>
